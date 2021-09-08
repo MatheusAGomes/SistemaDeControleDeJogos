18fe9a1249b2b229e1fccb8344e08fa2fcf903b6
MatheusAGomes <matheusgoassuncao@gmail.com>
2021-09-08 16:57:02 -0300

    P1 completo
    procedimento para guardar nome dos times efetuado

--- SistemaDeControleDeJogos.asm
+++ SistemaDeControleDeJogos.asm
@@ -7,12 +7,21 @@ msg4: .asciiz "\n 4 - para gerar resultados "
 msg5: .asciiz "\n Escolha: "
 msg6: .asciiz "\n O numero escolhido nao e uma escolha"
 msg7: .asciiz "\n Entre com o time "
-msg8: .asciiz ": "
+msg8: .asciiz " : "
+msg9: .asciiz "\n Selecione o primeiro time a fazer o resultado(Numero do time): "
+msg10: .asciiz "Numero do time \t Time \n"
+tab: .asciiz "\t  \t  "
+
+
+
 teste: .asciiz "\n Teste"
 
-times: .asciiz  "" , "" , "" , "" , "" , "" , "" , "" , "" , ""
+
 const : .word 10
 const2: .word 40
+
+
+times: .space 80 #10*8
 jogos: .word  0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0
 vitorias: .word  0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0
 derrotas: .word  0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0
@@ -21,6 +30,17 @@ derrotas: .word  0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0
 .text
 .globl main 
 main:
+
+
+jal menu
+
+
+li $v0,10
+syscall
+
+
+
+menu:
 
 li $v0, 4 # codigo para passar texto atraves do console syscall
 la $a0, msg1 # msg1 ser o objeto da escrita
@@ -70,44 +90,92 @@ addi $t4, $t0, -4			# $t0 = $t1 + 0
 beq $t4,$zero,P4
 
 
+P1:
 
-li $v0,5
+passagem_de_times:
+
+la		$t2, times 
+lw		$t1, const	# $t1 = 2
+li		$a1, 	8	# $a1 = 8
+add 	$s0,$zero,$zero #zerando t0 para loop
+		 
+
+loop_para_leitura_de_times:
+
+addi	$t4, $s0, 1			# $t3 = s01 1 0
+
+
+sll $t0,$s0,5 #multiplicando o indice por 32
+add $t3,$t2,$t0 #somando a multiplicacao com o endereco
+
+
+li $v0, 4 # codigo para passar texto atraves do console syscall
+la $a0, msg7 # msg1 ser o objeto da escrita
+syscall
+
+li $v0, 1 # codigo para passar texto atraves do console syscall
+move 	$a0, $t4		# $a0 = $31 # msg1 ser o objeto da escrita
+syscall
+
+li $v0, 4 # codigo para passar texto atraves do console syscall
+la $a0, msg8 # msg1 ser o objeto da escrita
 syscall
 
 
-P1:
-add $t0,$zero,$zero
-addi $t1,$zero,1
-lw  $t7, const2
+li		$v0,8 		# $v0 8= 
+la		$a0,0($t3) 		# $a0 0($t3)
+syscall
+addi $s0,$s0,1
+bne	$s0, $t1, loop_para_leitura_de_times	# if$s0 != $t1 then target
 
-
-loop:
-		beq $t0, $t7,main
-		li $v0, 4 # codigo para passar texto atraves do console syscall
-		la $a0, msg7 # msg1 ser o objeto da escrita
-		syscall
-        li $v0, 1 # codigo para passar texto atraves do console syscall
-		add $a0,$t1,$zero
-		syscall
-        li $v0, 4 # codigo para passar texto atraves do console syscall
-		la $a0, msg8 # msg1 ser o objeto da escrita
-		syscall
-
-        la $a0,times
-        li $a1,20
-		li $v0, 8 # Codigo para receber inteiros atraves do console syscall
-		syscall # valor colocado em $v0
-		sw $v0,times($t0)#recebendo valores do array
-		addi $t0,$t0,4
-        addi $t1,$t1,1
-	j loop
-
-
-
-
+j menu
 
 
 P2:
+
+passagem_de_times_mostrar:
+
+la		$t2, times 
+lw		$t1, const	# $t1 = 2
+li		$a1, 	8	# $a1 = 8
+add 	$s0,$zero,$zero #zerando t0 para loop
+
+li $v0, 4 # codigo para passar texto atraves do console syscall
+la $a0, msg10 # msg1 ser o objeto da escrita
+syscall
+		 
+
+loop_para_mostrar_times:
+
+addi	$t4, $s0, 1			# $t3 = s01 1 0
+
+
+sll $t0,$s0,5 #multiplicando o indice por 32
+add $t3,$t2,$t0 #somando a multiplicacao com o endereco
+
+
+li $v0, 1 # codigo para passar texto atraves do console syscall
+move 	$a0, $t4		# $a0 = $31 # msg1 ser o objeto da escrita
+syscall
+
+li $v0, 4 # codigo para passar texto atraves do console syscall
+la $a0, tab # msg1 ser o objeto da escrita
+syscall
+
+li		$v0,4		# $v0 8= 
+la		$a0,0($t3) 		# $a0 0($t3)
+syscall
+
+addi $s0,$s0,1
+bne	$s0, $t1, loop_para_mostrar_times	# if$s0 != $t1 then target
+
+li $v0, 4 # codigo para passar texto atraves do console syscall
+la $a0, msg9 # msg1 ser o objeto da escrita
+syscall
+
+
+
+
 
 P3:
 
